9081eda79e591b405791fd5360090ed73f5e7de7
Erik Olvera <erikolvera44@gmail.com>
2026-04-02 17:53:56 -0500

    create WatchList page component with basic routing structure

---
+++ src/Pages/WatchList.jsx
@@ -0,0 +1,10 @@
+import { useParams, Link } from "react-router-dom";
+import { supabase } from "../supabaseClient";
+
+export default function WatchList() {
+    return (
+        <div>
+            <h1>Watch List</h1>
+        </div>
+    )
+}
